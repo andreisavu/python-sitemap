48e8ab0bb4dc468b1ac510444d8f8b282fabade8
Andrei Savu <savu.andrei@gmail.com>
2009-08-05 19:10:38 +0300

    basic class for handlig urlset elements and some tests

--- sitemap/urlsetelement.py
+++ sitemap/urlsetelement.py
@@ -1,9 +1,70 @@
 
 from exceptions import *
 
-class UrlSetElement:
+class UrlSetElement(object):
+
+    def _immutable(self, v):
+        self._loc = 5
+        raise ImmutableException('This object is immutable.')
+
+    loc = property(lambda self:self._loc, _immutable)
+    lastmod = property(lambda self:self._lastmod, _immutable)
+    changefreq = property(lambda self:self._changefreq, _immutable)
+    priority = property(lambda self:self._priority, _immutable)
 
     def __init__(self, **kwargs):
-        pass
+        """ Init an urlset element. This object is immutable """
+        self._set_loc(kwargs)
+        self._set_lastmod(kwargs)
+        self._set_changefreq(kwargs)
+        self._set_priority(kwargs)
+        
+    def _set_loc(self, args):
+        """
+        Extract and validate location
+
+        This parameter is mandatory. It should be an absolute url.
+        The value must be less than 2048 characters.
+        """
+        if 'loc' not in args:
+            raise ValueError('loc parameter is mandatory')
+        # todo: validate url
+        self._loc = args['loc']
+
+    def _set_lastmod(self, args):
+        """ 
+        Extract and validate last modification date
+
+        This parameter is optional. It should be in the W3C Datetiem format. This 
+        format allows you to omit the time portion, if desired, and use YYYY-MM-DD
+        """
+        if 'lastmod' in args:
+            # todo: validate and transform in datetime object
+            self._lastmod = args['lastmod']
+        else:
+            self._lastmod = None
+
+    def _set_changefreq(self, args):
+        """
+        Extract and validate page change frequency
+
+        There is only a limited set of valid values. 
+        """
+        if 'changefreq' in args:
+            self._changefreq = args['changefreq']
+        else:
+            self._changefreq = None
+
+    def _set_priority(self, args):
+        """
+        Extract and validate page priority
+
+        The priority of urls is relative to other urls in the sitemap. 
+        """
+        if 'priority' in args:
+            self._priority = args['priority']
+        else:
+            self._priority = None
+
 
 
--- tests/test_urlsetelement.py
+++ tests/test_urlsetelement.py
@@ -3,5 +3,37 @@ import unittest
 from sitemap import *
 
 class TestUrlSetElement(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.record = {
+            'loc' : 'http://www.example.com',
+            'lastmod' : '2005-01-01',
+            'changefreq' : 'monthly',
+            'priority' : '0.3'
+        }
+
+    def testCreateOnlyWithLocation(self):
+        params = {
+            'loc':'http://www.example.com'
+        }
+        e = UrlSetElement(**params)
+
+    def testCreateFails_MissingLocation(self):
+        params = {
+            'changefreq' : 'daily',
+            'priority' : '0.5'
+        }
+        try:
+            e = UrlSetElement(**params)
+            self.assertTrue(False)
+        except ValueError:
+            pass
+
+    def testImmutableProperties(self):
+        e = UrlSetElement(**self.record)
+        try:
+            e.loc = 'Something'
+            self.assertTrue(False)
+        except ImmutableException:
+            pass
 
