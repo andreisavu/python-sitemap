0a9acf9434d16968f1118c281d024bbd6b1c9c60
Andrei Savu <savu.andrei@gmail.com>
2009-08-06 16:44:11 +0300

    basic lazy urlset reader working

--- sitemap/urlset.py
+++ sitemap/urlset.py
@@ -1,22 +1,68 @@
 
+from lxml import etree
+from cStringIO import StringIO
+from urllib import urlopen
+import os
+import re
+
 from exceptions import *
+from urlsetelement import *
 
 class UrlSet(object):
     """
     Sitemap urlset structure
 
-    Lazy loading of urlsets from sitemaps.  
+    Lazy loading of an urlset from a sitemap.
     """
 
     @staticmethod
+    def from_url(url):
+        """ Create an urlset from an url """
+        return UrlSet(urlopen(url), url)
+
+    @staticmethod
     def from_file(file):
-        pass
+        """ Create an urlset from a file """
+        return UrlSet(open(file), file)
+
+    @staticmethod
+    def from_str(str):
+        """ Create an urlset from a string """
+        return UrlSet(StringIO(str), 'string') 
 
     source = property(lambda self:self._source)
 
+    def __init__(self,handle, source='handle'):
+        """ Create an urlset from any kinf of File like object """
+        self._source = source
+        self._handle = handle
+
     def get_urls(self):
-        pass
+        """ Parse the xml file and generate the elements """
+        schema = etree.XMLSchema(file=open(self.get_schema_path()))
+        context = etree.iterparse(self._handle, events=('start',), schema=schema)
+
+        element_data = {}
+        for action, elem in context:
+            tag = self._remove_ns(elem.tag)
+            if tag == 'url' and element_data:
+                try:
+                    e = UrlSetElement(**element_data)
+                    yield e
+                except ValueError:
+                    element_data = {}
+                    continue
+            elif tag in ['loc', 'lastmod', 'changefreq', 'priority']:
+                element_data[tag] = elem.text
+            elem.clear()
+
+    def _remove_ns(self, str):
+        return re.sub('{[^}]*}', '', str)
+
+    def get_schema_path(self):
+        base = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(base, 'schemas', 'sitemap.xsd')
 
     def __iter__(self):
-        return self.get_urls()
+        return iter(self.get_urls())
 
--- tests/test_urlset.py
+++ tests/test_urlset.py
@@ -1,7 +1,20 @@
 
 import unittest
+import os
+from urlparse import urlparse
+
 from sitemap import *
 
-class TestUrlSet(unittest.TestCase):
-    pass
+class TestUrlSet(unittest.TestCase): 
+     
+    def setUp(self):
+        self.base_path = os.path.dirname(os.path.abspath(__file__))
+        self.small_sitemap = "%s/fixtures/sitemap.xml" % self.base_path
+
+    def testParseFile(self):
+        urlset = UrlSet.from_file(self.small_sitemap)
+        for url in urlset:
+            parts = urlparse(url.loc)
+            self.assertEquals(parts.netloc, 'www.example.com')
+            
 
