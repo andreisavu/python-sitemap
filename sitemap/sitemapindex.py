6b650b7e8084d396c76f920da3270adc6c3ca130
Andrei Savu <savu.andrei@gmail.com>
2009-08-06 18:56:51 +0300

    sitemap index parser

--- sitemap/sitemapindex.py
+++ sitemap/sitemapindex.py
@@ -1,36 +1,62 @@
 
+from lxml import etree
 from urllib import urlopen
 from cStringIO import StringIO
 
+from urlset import *
 from exceptions import *
 
 class SitemapIndex(object):
 
     @staticmethod
-    def from_url(url):
+    def from_url(url, **kwargs):
         """ Create a sitemap from an url """
-        return SitemapIndex(urlopen(url), url)
+        return SitemapIndex(urlopen(url), url, **kwargs)
 
     @staticmethod
-    def from_file(file):
+    def from_file(file, **kwargs):
         """ Create a sitemap from file """
-        return SitemapIndex(open(file), file)
+        return SitemapIndex(open(file), file, **kwargs)
 
     @staticmethod
-    def from_str(str):
+    def from_str(str, **kwargs):
         """ Create a sitemap from a string """
-        return SitemapIndex(StringIO(str), 'string')
+        return SitemapIndex(StringIO(str), 'string', **kwargs)
 
     source = property(lambda self:self._source)
 
-    def __init__(self, handle, source='handle'):
+    def __init__(self, handle, source='handle', validate=True):
         self._source = source
         self._handle = handle
+        self._validate = validate
 
-    def get_sitemaps(self):
+    def get_urlsets(self):
         """ Parse the xml file and generate the urlsets. """
-        
+        if self._validate:
+            schema = etree.XMLSchema(file=open(self.get_schema_path()))
+        else:
+            schema = None
+        context = etree.iterparse(self._handle, events=('start',), schema=schema)
+
+        location = ''
+        for action, elem in context:
+            tag = self._remove_ns(elem.tag)
+            if tag == 'sitemap' and location:
+                try:
+                    yield UrlSet.from_url(location, validate=self._validate)
+                except:
+                    location = ''
+                    continue
+            elif tag == 'loc':
+                location = elem.text
+
+    def _remove_ns(self, str):
+        return re.sub('{[^}]*}', '', str)
+
+    def get_schema_path(self):
+        base = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(base, 'schemas', 'siteindex.xsd')
 
     def __iter__(self):
-        return iter(self.get_sitemaps())
+        return iter(self.get_urlsets())
 
--- tests/test_sitemapindex.py
+++ tests/test_sitemapindex.py
@@ -1,7 +1,19 @@
 
+import os
 import unittest
+from urlparse import urlparse
+
 from sitemap import *
 
 class TestSitemapIndex(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.base = os.path.dirname(os.path.abspath(__file__))
+        self.sitemap_index = os.path.join(self.base, 'fixtures', 'sitemap-index.xml')
+
+    def testParse(self):
+        index = SitemapIndex.from_file(self.sitemap_index)
+        for urlset in index:
+            parts = urlparse(urlset.source)
+            self.assertEquals(parts.netloc, 'www.example.com')
 
