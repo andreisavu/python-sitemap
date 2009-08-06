5b0df9fb2bef6425a8fbf5a07ed1d45ea1dfb170
Andrei Savu <savu.andrei@gmail.com>
2009-08-06 19:05:56 +0300

    test parsing of broken sitemap index

--- tests/test_sitemapindex.py
+++ tests/test_sitemapindex.py
@@ -2,6 +2,7 @@
 import os
 import unittest
 from urlparse import urlparse
+from lxml.etree import XMLSyntaxError
 
 from sitemap import *
 
@@ -9,11 +10,21 @@ class TestSitemapIndex(unittest.TestCase):
     
     def setUp(self):
         self.base = os.path.dirname(os.path.abspath(__file__))
-        self.sitemap_index = os.path.join(self.base, 'fixtures', 'sitemap-index.xml')
+        self.fixtures = os.path.join(self.base, 'fixtures')
 
-    def testParse(self):
-        index = SitemapIndex.from_file(self.sitemap_index)
-        for urlset in index:
+        self.sitemap_index = os.path.join(self.fixtures, 'sitemap-index.xml')
+        self.broken_sitemap = os.path.join(self.fixtures, 'broken-sitemap-index.xml')
+
+    def checkSitemapIndex(self, siteindex):
+        for urlset in siteindex:
             parts = urlparse(urlset.source)
             self.assertEquals(parts.netloc, 'www.example.com')
-
+ 
+    def testParse(self):
+        index = SitemapIndex.from_file(self.sitemap_index)
+        self.checkSitemapIndex(index)
+       
+    def testParseFail_NotValid(self):
+        index = SitemapIndex.from_file(self.broken_sitemap)
+        self.assertRaises(XMLSyntaxError, self.checkSitemapIndex, index)
+        
