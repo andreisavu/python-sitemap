d67132de6d634a1edb7780941a1ee57dfc50bf2e
Andrei Savu <savu.andrei@gmail.com>
2009-08-06 17:00:51 +0300

    add test for loading from string & small refactor

--- tests/test_urlset.py
+++ tests/test_urlset.py
@@ -11,10 +11,17 @@ class TestUrlSet(unittest.TestCase):
         self.base_path = os.path.dirname(os.path.abspath(__file__))
         self.small_sitemap = "%s/fixtures/sitemap.xml" % self.base_path
 
-    def testParseFile(self):
-        urlset = UrlSet.from_file(self.small_sitemap)
+    def checkContent(self, urlset):
         for url in urlset:
             parts = urlparse(url.loc)
             self.assertEquals(parts.netloc, 'www.example.com')
-            
+       
+    def testParseFile(self):
+        urlset = UrlSet.from_file(self.small_sitemap)
+        self.checkContent(urlset)
+       
+    def testParseString(self):
+        content = open(self.small_sitemap).read()
+        urlset = UrlSet.from_str(content)
+        self.checkContent(urlset)
 
