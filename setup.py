de2d2dac2c66a980a9f01a7d73efd53411817ece
Andrei Savu <savu.andrei@gmail.com>
2011-04-18 22:05:11 +0300

    making the code available under the Apache License, version 2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
       version='0.1.0',
       description='Python library for parsing and generating sitemaps',
       author='Andrei Savu',
-      author_email='contact@andreisavu.ro',
+      author_email='asavu@apache.org',
       url='http://github.com/andreisavu/python-sitemap/tree/master',
       packages=['sitemap'],
       package_data={'sitemap' : ['schemas/*.xsd']}
